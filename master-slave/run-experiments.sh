57f114596deb4bf371fcd82a9dd5f061ac33dbad
Claudio Scheer <claudioscheer@protonmail.com>
2020-10-15 15:47:57 -0300

    Script to run experiments

---
+++ master-slave/run-experiments.sh
@@ -0,0 +1,6 @@
+wget http://gmap.pucrs.br/claudioscheer/mpic++-9.3.0.tar
+wget https://gmap.pucrs.br/claudioscheer/gcc-9.3.0.tar
+tar -xvf mpic++-9.3.0.tar
+tar -xvf gcc-9.3.0.tar
+
+source ./gcc-9.3.0/activate
